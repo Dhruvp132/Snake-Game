c932f8279fb0912897a39c4f942e457849e826e6
Dhruvp132 <dhruvpatel1321@gmail.com>
2023-11-19 19:57:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Board from './Board/Board';
 function App() {
   return (
     <div className="App">
-      
+      <Board></Board>
     </div>
   );
 }
